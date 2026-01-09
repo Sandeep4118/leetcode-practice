1c233bf9253aeaef48168c3a172102afd34742ad
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-01-09 22:21:00 +0530

    Time: 1 ms (99.24%), Space: 45.2 MB (92.58%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,15 +1,30 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] arr = new int[nums.length];
-        for(int i = 0; i<nums.length; i++){
-            int count = 0;
-            for(int j = 0; j<nums.length; j++){
-                if(nums[j]<nums[i]){
-                    count++;
-                }
-            }
-            arr[i] = count;
+
+        // Step 1: Frequency array
+        int[] count = new int[101];   // because 0 ≤ nums[i] ≤ 100
+
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
         }
-        return arr;
+
+        // Step 2: Prefix sum
+        // count[i] = how many numbers are <= i
+        for (int i = 1; i < 101; i++) {
+            count[i] = count[i] + count[i - 1];
+        }
+
+        // Step 3: Build answer array
+        int[] newarr = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                newarr[i] = 0;
+            } else {
+                newarr[i] = count[nums[i] - 1];
+            }
+        }
+
+        return newarr;
     }
 }
