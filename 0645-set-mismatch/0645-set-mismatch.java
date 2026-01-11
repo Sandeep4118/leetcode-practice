96e244074a87f112bad2e8c4272ca902ae74a1be
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-01-11 16:59:17 +0530

    Time: 20 ms (10.98%), Space: 48.1 MB (13.16%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,18 +1,40 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] arr = new int[nums.length+1];
-        for(int i = 0; i<nums.length; i++)
-        {
-            arr[nums[i]]++;
-        }
+        // int[] arr = new int[nums.length+1];
+        // for(int i = 0; i<nums.length; i++)
+        // {
+        //     arr[nums[i]]++;
+        // }
+        // int duplicate = 0;
+        // int missing = 0;
+
+        // for(int i = 1; i<=nums.length; i++){
+        //     if(arr[i]==2){
+        //         duplicate = i;
+        //     }
+        //     else if(arr[i]==0){
+        //         missing = i;
+        //     }
+        // }
+        // return new int[]{duplicate, missing};
         int duplicate = 0;
         int missing = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            int value = map.getOrDefault(nums[i],0);
+            map.put(nums[i],value+1);
+        }
 
-        for(int i = 1; i<=nums.length; i++){
-            if(arr[i]==2){
-                duplicate = i;
+        for(Map.Entry<Integer, Integer> it: map.entrySet()){
+            if(it.getValue() == 2){
+                duplicate = it.getKey();
             }
-            else if(arr[i]==0){
+        }
+
+        // int k = 0;
+        for(int i = 1; i<=nums.length; i++){
+            if(map.containsKey(i))continue;
+            else{
                 missing = i;
             }
         }
