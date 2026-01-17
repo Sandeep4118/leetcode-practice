7bb3064aedfd227871daf90feab2ccff4375ebe1
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-01-18 02:48:58 +0530

    Time: 0 ms (100%), Space: 44.5 MB (18.52%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,46 +1,61 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String> str = new ArrayList<>();
-        Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // List<String> str = new ArrayList<>();
+        // Stack<Integer> st = new Stack<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
 
-        for(int i = 0; i<target.length; i++){
-            int value = map.getOrDefault(target[i],0);
-            map.put(target[i],value+1);
-        }
+        // for(int i = 0; i<target.length; i++){
+        //     int value = map.getOrDefault(target[i],0);
+        //     map.put(target[i],value+1);
+        // }
 
 
-        for(int i = 1; i<=n; i++){
-            st.push(i);
-            str.add("Push");
+        // for(int i = 1; i<=n; i++){
+        //     st.push(i);
+        //     str.add("Push");
 
-            if(!map.containsKey(i)){
-                st.pop();
-                str.add("Pop");
-            }
-
-
-            boolean match = true;
-
-            if(st.size() != target.length){
-                match = false;
-            }
-            else{
-                for(int j = 0; j<target.length; j++){
-                    if(st.get(j) != target[j]){
-                        match = false;
-                        break;
-                    }
-                }
-            }
+        //     if(!map.containsKey(i)){
+        //         st.pop();
+        //         str.add("Pop");
+        //     }
 
 
-            if(match == true){
-                break;
-            }
+        //     boolean match = true;
+
+        //     if(st.size() != target.length){
+        //         match = false;
+        //     }
+        //     else{
+        //         for(int j = 0; j<target.length; j++){
+        //             if(st.get(j) != target[j]){
+        //                 match = false;
+        //                 break;
+        //             }
+        //         }
+        //     }
+
+
+        //     if(match == true){
+        //         break;
+        //     }
             
+        // }
+        // return str;
+
+        List<String> arr = new ArrayList<>();
+        int j = 0;
+        for(int i = 1; i<=n; i++){
+            if(j==target.length) break;
+            if(target[j] == i){
+                arr.add("Push");
+                j++;
+            }
+            else if(target[j] != i){
+                arr.add("Push");
+                arr.add("Pop");
+            }
         }
-        return str;
+        return arr;
     }
 }
