c6f5ab129fd68e8ba61d084036def1b8522f7652
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-01-18 23:47:35 +0530

    Time: 1 ms (97.7%), Space: 44 MB (29.51%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
@@ -1,19 +1,24 @@
 class Solution {
     public int minOperations(List<Integer> nums, int k) {
         ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 1; i<=k; i++){
-            arr.add(i);
-        }
-
+        // for(int i = 1; i<=k; i++){
+        //     arr.add(i);
+        // }
+        
         int count = 0;
         for(int i = nums.size()-1; i>=0; i--){
             
-            if(arr.size()==0)  break;
-            count++;
+            // if(arr.size()==0)  break;
+            // count++;
 
-            if(arr.contains(nums.get(i))){
-                arr.remove(Integer.valueOf(nums.get(i)));
-            }
+            // if(arr.contains(nums.get(i))){
+            //     arr.remove(Integer.valueOf(nums.get(i)));
+            // }
+            if(arr.size()==k){
+                return count;}
+            count++;
+            if(nums.get(i)<=k&& !arr.contains(nums.get(i)))arr.add(nums.get(i));
+            
 
         }
         return count;
