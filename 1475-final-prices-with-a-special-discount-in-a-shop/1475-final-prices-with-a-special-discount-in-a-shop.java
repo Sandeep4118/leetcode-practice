912e21351ec4db4337a49d1810994f224d29099f
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-02-08 22:18:43 +0530

    Time: 4 ms (51.44%), Space: 45.5 MB (21.12%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,29 +1,18 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        // for(int i = 0; i<prices.length; i++){
-        //     for(int j = i+1; j<prices.length; j++){
-        //         if(j>i && prices[j]<= prices[i]){
-        //             prices[i] = Math.abs(prices[j]-prices[i]);
-        //             break;
-        //         }
-        //     }
-        // }
-        // return prices;
-
-
-        int [] result = new int[prices.length];
         Stack<Integer> st = new Stack<>();
+        int [] arr = new int[prices.length];
         for(int i = 0; i<prices.length; i++){
             while(!st.isEmpty() && prices[st.peek()]>=prices[i]){
                 int index = st.pop();
-                result[index] = prices[index] - prices[i];
+                arr[index] = prices[index] - prices[i];
             }
             st.push(i);
         }
         while(!st.isEmpty()){
             int index2 = st.pop();
-            result[index2] = prices[index2];
+            arr[index2] = prices[index2];
         }
-        return result;
+        return arr;
     }
 }
