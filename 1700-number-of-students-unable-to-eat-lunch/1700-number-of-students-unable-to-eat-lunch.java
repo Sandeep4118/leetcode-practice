93edb24cd188e21de3680d0ec7704625cb3c96ff
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-02-09 06:19:31 +0530

    Time: 1 ms (51.8%), Space: 43.3 MB (53.04%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,30 +1,47 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i = 0; i<students.length; i++){
+        //     q.offer(students[i]);
+        // }
+
+        // Stack<Integer> st = new Stack<>();
+        // for(int i = sandwiches.length-1; i>=0; i--){
+        //     st.push(sandwiches[i]);
+        // }
+
+        // int count = 0;
+        // while(!q.isEmpty()){
+        //     if(count == q.size()) break;
+
+        //     if(st.peek().equals(q.peek())){
+        //         st.pop();
+        //         q.poll();
+        //         count = 0;
+        //     }
+        //     else{
+        //         q.offer(q.poll());
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i<students.length; i++){
-            q.offer(students[i]);
+            int value = map.getOrDefault(students[i],0);
+            map.put(students[i],value+1);
         }
+        
+        for(int i = 0; i<sandwiches.length; i++){
+            int type = sandwiches[i];
 
-        Stack<Integer> st = new Stack<>();
-        for(int i = sandwiches.length-1; i>=0; i--){
-            st.push(sandwiches[i]);
-        }
-
-        int count = 0;
-        while(!q.isEmpty()){
-            if(count == q.size()) break;
-
-            if(st.peek().equals(q.peek())){
-                st.pop();
-                q.poll();
-                count = 0;
+            if(map.getOrDefault(type,0)!=0){
+                map.put(type,map.get(type)-1);
             }
             else{
-                q.offer(q.poll());
-                count++;
+                return sandwiches.length - i;
             }
         }
-        return q.size();
-
+        return 0;
     }
 }
