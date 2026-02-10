e723dd0921ab188fe3a5b7dcdde1b60272c39fdd
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-02-11 03:07:04 +0530

    Time: 0 ms (100%), Space: 43.2 MB (41.79%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,23 +1,30 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<int[]> q = new LinkedList<>();
-        for(int i = 0; i<tickets.length; i++){
-            q.offer(new int[]{i,tickets[i]});
-        }
+        // Queue<int[]> q = new LinkedList<>();
+        // for(int i = 0; i<tickets.length; i++){
+        //     q.offer(new int[]{i,tickets[i]});
+        // }
+
+        // int time = 0;
+        // while(!q.isEmpty()){
+        //     int[] cur = q.poll();
+        //     int first = cur[0];
+        //     int second = cur[1];
+
+        //     time++;
+        //     second--;
+
+        //     if(first==k && second==0) break;
+        //     if(second>0){
+        //         q.offer(new int[]{first,second});
+        //     }
+        // }
+        // return time;
 
         int time = 0;
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int first = cur[0];
-            int second = cur[1];
-
-            time++;
-            second--;
-
-            if(first==k && second==0) break;
-            if(second>0){
-                q.offer(new int[]{first,second});
-            }
+        for(int i = 0; i<tickets.length; i++){
+            if(i<=k) time += Math.min(tickets[i],tickets[k]);
+            else if(i>k) time += Math.min(tickets[k]-1,tickets[i]);
         }
         return time;
     }
