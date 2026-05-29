69311a41ff9f041896ae036057c7b3f5209fbfe9
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-05-29 18:52:41 +0530

    Time: 21 ms (92.82%), Space: 53.9 MB (50.35%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -3,46 +3,41 @@ class Solution {
         int m = str1.length();
         int n = str2.length();
 
-        int[][] t = new int [m+1][n+1];
+        int[][]t = new int[m+1][n+1];
         for(int i = 0; i<m+1; i++){
             for(int j = 0; j<n+1; j++){
-                if(i==0) t[i][j] = 0;
-                if(j==0) t[i][j] = 0;
+                if(i==0)t[i][j] = 0;
+                if(j==0)t[i][j] = 0;
             }
         }
 
-        // filling the rows and columns for i = 1 , j = 1
         for(int i = 1; i<m+1; i++){
             for(int j = 1; j<n+1; j++){
-                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
                     t[i][j] = 1 + t[i-1][j-1];
                 }
                 else{
-                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+                    t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
                 }
             }
         }
 
-        // traversing from last element of the matrics 
         StringBuilder str = new StringBuilder();
         int i = m;
         int j = n;
-
         while(i>0 && j>0){
             if(str1.charAt(i-1) == str2.charAt(j-1)){
                 str.append(str1.charAt(i-1));
                 i--;
                 j--;
             }
+            else if(t[i-1][j]>t[i][j-1]){
+                str.append(str1.charAt(i-1));
+                i--;
+            }
             else{
-                if(t[i-1][j] > t[i][j-1]){
-                    str.append(str1.charAt(i-1));
-                    i--;
-                }
-                else{
-                    str.append(str2.charAt(j-1));
-                    j--;
-                }
+                str.append(str2.charAt(j-1));
+                j--;
             }
         }
         while(i>0){
