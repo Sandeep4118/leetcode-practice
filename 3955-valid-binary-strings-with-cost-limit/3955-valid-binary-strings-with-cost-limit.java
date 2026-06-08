5a8a1416089dd667aeeb74aeb5023ddfc67b6e15
Sandeep kumar Tiwari <144949184+Sandeep4118@users.noreply.github.com>
2026-06-08 19:27:59 +0530

    Time: 11 ms (100%), Space: 47.1 MB (100%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.java
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.java
@@ -1,25 +1,23 @@
 class Solution {
-    public static void recur(int idx, int prev, int cost, List<String>str,int n, String str1, int k){
-        if(cost>k){
-            return;
-        }
-        if(idx==n){
-            str.add(str1);
+    public static void recur(int index, int prev, int cost, List<String> result, String str, int n, int k){
+        if(cost>k)return;
+        if(index==n){
+            result.add(str);
             return;
         }
 
-        // non-pick t o 0
-        recur(idx+1,0,cost,str,n,str1+"0",k);
+        // take a 0 means non-pick;
+        recur(index+1,0,cost,result, str+"0",n,k);
 
-        // pick to 1
+        // take a 1 means pick
         if(prev != 1){
-            recur(idx+1, 1, cost+idx, str,n,str1+"1",k);
+            recur(index+1,1,cost+index,result, str+"1",n,k);
         }
     }
     public List<String> generateValidStrings(int n, int k) {
-        List<String> str = new ArrayList<>();
-        String str1 = "";
-        recur(0,0,0,str,n,str1,k);
-        return str;
+        List<String> result = new ArrayList<>();
+        String str = "";
+        recur(0,0,0,result,str,n,k);
+        return result;
     }
 }
